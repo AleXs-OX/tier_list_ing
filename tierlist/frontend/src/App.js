53db90863d5781c4608025eaf2601fc10dd27bb7
jmorlp00 <55236772+jmorlp00@users.noreply.github.com>
2022-05-11 13:42:40 +0200

    Pagina principal
    Frontend de la página principal casi terminado

---
+++ tierlist/frontend/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
+
+import Login from './pages/principal.js'
+
+
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Navigate to="/principal"/>}/>
+        <Route path="/principal" element={<Login/>}/>
+
+      </Routes>
+    </div>
+    </Router>
+    
+  );
+}
+
+export default App;
